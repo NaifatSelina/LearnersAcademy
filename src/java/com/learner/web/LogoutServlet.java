68c572dce73dd5e49de292f96f6d8484c7a56edb
NaifatSelina <naifatselina@icloud.com>
2022-11-07 13:58:53 +0000

    add Servlet implementation class LogoutServlet to LogoutServlet.java

---
+++ src/java/com/learner/web/LogoutServlet.java
@@ -0,0 +1,34 @@
+package com.learner.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 102831973239L;
+
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("Logged out successfully<br>");
+        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+        rd.include(request, response);
+        out.println("</body></html>");
+        out.close();
+    }
+
+}
