670b4e77fbbfa544bc081ec629bdd9ffe30c5365
NaifatSelina <naifatselina@icloud.com>
2022-11-07 12:43:12 +0000

    add user details to sql file

--- LearnersAcademy.sql
+++ LearnersAcademy.sql
@@ -1,8 +1,15 @@
-create database learnersacademy;
-use learnersacademy;
+create database learners-academy;
+use learners-academy;
 
 CREATE TABLE user (username VARCHAR(100), password VARCHAR(100));
 CREATE TABLE grade (name VARCHAR(100));
 CREATE TABLE subject (name VARCHAR(100), classId INT);
 CREATE TABLE teacher (id INT, name VARCHAR(100), classId INT);
 CREATE TABLE student (id INT, name VARCHAR(100), classId INT);
+
+insert into user(username, password)
+values('admin','admin');
+insert into user(username, password)
+values('naifat','password');
+
+
